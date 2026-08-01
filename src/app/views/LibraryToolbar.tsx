4599e3831fe8f9f267aa6473deac744e1936df21
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2026-08-01 20:54:49 +0300

    Consolidate app styles into layered domains

--- src/app/views/LibraryToolbar.tsx
+++ src/app/views/LibraryToolbar.tsx
@@ -65,7 +65,7 @@ export function LibraryToolbar () {
 
         <li>
           <fieldset className='density-toggle'>
-            <legend>Row density</legend>
+            <legend className='sr-only'>Row density</legend>
 
             {DENSITIES.map(densityOption =>
               <label key={densityOption} title={`${densityOption} density`}>
